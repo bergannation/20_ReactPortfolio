3823cbab71894f77cb2bf4ccabd6296937aaf13e
Travis Brown <bergannation@gmail.com>
2021-08-05 15:24:00 -0500

    adding images/css/js from old portfolio

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import NavTabs from './NavTabs';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
-import Contact from './pages/Contact';
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState("Home");
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
+    if (currentPage === "Home") {
       return <Home />;
     }
-    if (currentPage === 'About') {
+    if (currentPage === "About") {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === "Projects") {
+      return <Projects />;
     }
     return <Contact />;
   };
